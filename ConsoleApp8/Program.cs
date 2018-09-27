d1ddacc31892a83c2392a56cf912cdb1cda4ffe0
Ryan <werner.ryan18@gmail.com>
2018-09-27 14:11:52 -0700

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main()
+        {
+            string holder;
+            int number = 0;
+
+            while (number != 3)
+            {
+                Console.WriteLine("enter 3 or else!");
+                holder = Console.ReadLine();
+                number = Convert.ToInt32(holder);
+            }
+
+            do
+            {
+                Console.WriteLine("enter 20 or else!");
+                holder = Console.ReadLine();
+                number = Convert.ToInt32(holder);
+            } while (number != 20);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
